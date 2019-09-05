e8f6e7605c18ed35717aef93d38597b0fd5edf1f
Juan Pablo Sanchez <jpsanchez@inti-moda.com>
2019-09-05 17:59:06 -0500

    commit from work 2019-09-05 17:58

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+import { TabPanel } from 'primeng/components/tabview/tabview';
 
 @Component({
   selector: 'app-home',
@@ -12,11 +13,19 @@ export class HomeComponent implements OnInit {
   items2: MenuItem[];
 
   activeItem: MenuItem;
+
+  //items: TabPanel
+
+  //items: any = [{ header: 'tabbdfd', selected: false, disabled: false, closable: true, closed: false, content: 'app-cambio-clave' }, { header: '3333', selected: false, disabled: false, closable: true, closed: false, content: 'camboya' }];
+
+  items: any = [];
+
   constructor() { }
 
   ngOnInit() {
+    /*
     this.items1 = [
-      { label: 'Stats', icon: 'fa fa-fw fa-bar-chart' },
+      { label: 'Stats', icon: 'fa fa-fw fa-bar-chart', routerLink: ['/cambio-clave'] },
       { label: 'Calendar', icon: 'fa fa-fw fa-calendar' },
       { label: 'Documentation', icon: 'fa fa-fw fa-book' },
       { label: 'Support', icon: 'fa fa-fw fa-support' },
@@ -32,11 +41,17 @@ export class HomeComponent implements OnInit {
     ];
 
     this.activeItem = this.items2[0];
+    */
   }
 
   closeItem(event, index) {
     this.items2 = this.items2.filter((item, i) => i !== index);
     event.preventDefault();
     this.items2.push({ label: 'Socialite france', icon: 'fa fa-fw fa-twitter' })
+  }
+
+  handleClick(event) {
+    alert('xdd');
+    this.items.push({ header: 'xyy', selected: false, disabled: false, closable: true, closed: false, content: 'app-mantenimiento-perfil' });
   }
 }
