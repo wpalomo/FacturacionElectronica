1a603325ad5a0e11f9c45de63923f27f1f0034fb
Juan Pablo Sanchez <jpsanchez@inti-moda.com>
2019-09-25 18:26:58 -0500

    commit from work 2019-09-25 18:26

--- src/app/components/general/favoritos/favoritos.component.ts
+++ src/app/components/general/favoritos/favoritos.component.ts
@@ -1,4 +1,4 @@
-import {  Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { MenuFavoritosService } from '../../../services/menu-favoritos/menu-favoritos.service';
@@ -12,6 +12,8 @@ import ITB_GEN_FAVORITOS from 'src/app/model/ITB_GEN_FAVORITOS';
 export class FavoritosComponent implements OnInit {
   favoritos: ITB_GEN_FAVORITOS[];
   favorito: ITB_GEN_FAVORITOS = {};
+  selectedFavoritos: ITB_GEN_FAVORITOS[];
+  cols: any[];
 
   constructor(
     private menuFavoritosService: MenuFavoritosService
@@ -22,8 +24,51 @@ export class FavoritosComponent implements OnInit {
       data => {
         this.favoritos = data;
         console.log(this.favoritos);
+        console.log('despues');
+        console.log(this.favoritos[0]);
+
+        let selected: ITB_GEN_FAVORITOS[] = this.favoritos.filter(favorito => favorito.acceso == 'S');
+
+        this.selectedFavoritos = this.favoritos.filter(favorito => favorito.acceso == 'S');
+
+        /*
+        console.log(selected);
+
+        for (let i = 0; i < this.favoritos.length; i++) {
+          console.log(this.favoritos[i].acceso);
+          this.selectedFavoritos.push(this.favoritos[i]);
+          //var currentNumber = numbers[i];
+          //if (currentNumber > 10) {
+          //  greaterTen.push(currentNumber)
+          //}
+        }
+        */
+        //this.selectedFavoritos = [selected];
+        //this.selectedFavoritos.push({
+        //  "id_menu_favoritos": 1
+        //});
       }
     );
+
+    this.cols = [
+      {
+        field: 'id_menu_favoritos',
+        header: 'Codigo',
+        width: '10%'
+      },
+      {
+        field: 'nombre_menu',
+        header: 'Menu',
+        width: '40%'
+      }
+    ];
+
+
+
+    //this.selectedFavoritos.push({
+    //  id_menu_favoritos: 50, id_menu: 5, nombre_menu: "Cambio de Clave", acceso: "S", estado_menu_favoritos: "A"
+    //})
+    //this.selectedFavoritos.id_menu_favoritos = 2;
   }
 
 }
